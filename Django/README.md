879d4fdd42f9f687f025d51bf1b3d1e97286cb2b
sjy9412 <sjy9412@gmail.com>
2019-12-30 00:02:26 +0900

    191229|SJY|Django Add serializer

--- Django/README.md
+++ Django/README.md
@@ -1,5 +1,16 @@
 # Django
 
+* 이미지 크기 일정하게 저장하기 위해 imagekit 설치
+
+  ```bash
+$pip install pillow
+  $pip install pilkit
+# 순서대로 설치
+  $pip install django-imagekit
+```
+  
+  
+  
 * model
 
   * 게시글 - 사진, 내용, 좋아요, 해쉬태그, 등록시간, 유저
@@ -8,4 +19,5 @@
 
   * 해쉬태그 - 내용
 
-    
+  
+  
---
+++ Django/articles/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Hashtag, Article, Comment
+
+class HashtagSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Hashtag
+        fields = ['content',]
+
+class ArticleSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'image_thumbnail', 'content', 'created_at', 'like_user', 'user', 'hashtag']
+
+class CommentSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['content', 'created_at']
+
+class ArticleDeailSerializer(serializers.ModelSerializer):
+    comment_set = CommentSerializers(many=True)
+    class Meta(ArticleSerializers.Meta):
+        fields = ArticleSerializers.Meta.fields + ['comment_set']
